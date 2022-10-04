cde35f8b52fc7696b2a2f3d09f7dbdfe838f4d63
simonesanna1983 <35135396+simonesanna1983@users.noreply.github.com>
2022-10-04 12:22:26 +0200

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,14 +1,32 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+
 import './style.css';
 
 export default function App() {
-  const { t } = useTranslation();
+  const [language, setLanguage] = useState('en');
+  const { t, i18n } = useTranslation();
+
+  function handleChange(event) {
+    setLanguage(event.target.value);
+
+    i18n.changeLanguage('it');
+  }
 
   return (
     <div>
       <h1>Hello</h1>
+      <p>{language}</p>
       <h1>{t('Hello')}</h1>
+
+      <select value={language} onChange={handleChange}>
+        <option value="en">English</option>
+        <option value="es">Spanish</option>
+        <option value="it">Italian</option>
+        <option value="ja">Japanese</option>
+        <option value="el">greek</option>
+      </select>
     </div>
   );
 }
