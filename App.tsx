d390949539985eca74c8a75d55d6682c44e629c4
simonesanna1983 <35135396+simonesanna1983@users.noreply.github.com>
2022-10-04 11:44:56 +0200

    1 binaries uploaded

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
 import './style.css';
 
 export default function App() {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello</h1>
+      <h1>{t('Hello')}</h1>
     </div>
   );
 }
